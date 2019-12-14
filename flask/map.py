a6e9e1acf2551a3ac42a9bc2061f1fd67ba132c5
Roman <roma0398@mail.ru>
2019-12-14 12:34:12 +0300

    change marker names on map

--- flask/map.py
+++ flask/map.py
@@ -42,7 +42,8 @@ def map_render(start_date, end_date):
                 continue
             if round(location[0], 6) == spb[0] and round(location[1], 6) == spb[1]:
                 continue
-            folium.Marker(location=[location[0], location[1]], popup = folium.Popup(f'<a href="https://vk.com/spb_today?w=wall-68471405_{id}"target="_blank">link to news in VK</a>'), icon=folium.Icon(color = 'red', icon='info-sign')).add_to(marker_cluster)
+            name = df['pavlov'][i]
+            folium.Marker(location=[location[0], location[1]], popup = folium.Popup(f'<a href="https://vk.com/spb_today?w=wall-68471405_{id}"target="_blank">{name}</a>'), icon=folium.Icon(color = 'red', icon='info-sign')).add_to(marker_cluster)
     map.save("templates/map.html")
 
 class ExampleForm(FlaskForm):
